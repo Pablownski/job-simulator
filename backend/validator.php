c70d01de8ddde40bb546fb1143394f4f12cb9ece
Pablownski <sal24374@uvg.edu.gt>
2026-03-19 20:16:54 -0600

    se creo el validador para que los datos si cumplan su tipo de datos.

---
+++ backend/validator.php
@@ -0,0 +1,23 @@
+<?php
+
+function validate($data){
+    if (
+        !isset(
+            $data["campo1"],
+            $data["campo2"],
+            $data["campo3"],
+            $data["campo4"],
+            $data["campo5"],
+            $data["campo6"],
+        )
+    ){
+        return false;
+    }
+    
+    return is_string($data["campo1"]) &&
+           is_string($data["campo2"]) &&
+           is_string($data["campo3"]) &&
+           is_int($data["campo4"]) &&
+           (is_float($data["campo5"]) || is_numeric($data["campo5"])) &&
+           is_boolean($data["campo6"]);
+}
